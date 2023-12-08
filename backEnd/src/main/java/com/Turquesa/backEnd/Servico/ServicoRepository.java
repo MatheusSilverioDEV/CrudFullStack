e5ad65cb2a90757b84d8f79ed3d03925c1208dde
Matheus Silvério <matheussilveriodev@outlook.com>
2023-12-08 14:55:07 -0300

    Atualizando exclusão de servicos após excluir categoria

--- backEnd/src/main/java/com/Turquesa/backEnd/Servico/ServicoRepository.java
+++ backEnd/src/main/java/com/Turquesa/backEnd/Servico/ServicoRepository.java
@@ -1,6 +1,12 @@
 package com.Turquesa.backEnd.Servico;
 
+import com.Turquesa.backEnd.categoria.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ServicoRepository extends JpaRepository<Servico, Long> {
+
+    List<Servico> findByCategorias(Categoria categoria);
+
 }
--- backEnd/src/main/java/com/Turquesa/backEnd/categoria/CategoriaController.java
+++ backEnd/src/main/java/com/Turquesa/backEnd/categoria/CategoriaController.java
@@ -1,5 +1,8 @@
 package com.Turquesa.backEnd.categoria;
 
+import com.Turquesa.backEnd.Servico.Servico;
+import com.Turquesa.backEnd.Servico.ServicoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,9 @@ public class CategoriaController {
 
     @Autowired
     private CategoriaRepository repository;
+
+    @Autowired
+    private ServicoRepository servicoRepository;
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
@@ -37,13 +43,24 @@ public class CategoriaController {
         categoria.atualizar(dados);
         return ResponseEntity.ok().build();
     }
+
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @DeleteMapping@Transactional
-    public ResponseEntity deletarCategoria(@RequestBody Map<String, Long> requestbody){
+    public ResponseEntity deletarCategoria(@RequestBody Map<String, Long> requestbody) {
         Long id = requestbody.get("id");
+
+        Categoria categoria = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada com o ID: " + id));
+
+        List<Servico> servicos = servicoRepository.findByCategorias(categoria); // Removendo categoria dos serviços que está associada
+        for (Servico servico : servicos) {
+            servico.getCategorias().remove(categoria);
+        }
+
         repository.deleteById(id);
+
         return ResponseEntity.ok().build();
     }
-
-
 }
+
+
